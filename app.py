1c667bb8c7b3246fa909d24deb79529b3fc981a6
Noah-Abraham <nkabraham28@gmail.com>
2025-06-03 12:10:18 -0400

    fixed display issues

--- app.py
+++ app.py
@@ -192,7 +192,7 @@ if bolts:
         elif view_option == "XZ View":
             ax.quiver(x, 0, vx * vector_display_scale, vz * vector_display_scale, angles='xy', scale_units='xy', scale=1, color='green')
             ax.plot(x, 0, 'ro')
-            label_text = ' & '.join(f"Bolt {j}" for j in bolt_positions[(round(x, 3), round(vz * vector_display_scale, 3))])
+            label_text = ' & '.join(f"Bolt {j}" for j in bolt_positions[(round(x, 3), round(0, 3))])
             offset_x = -0.25 if vx >= 0 else 0.25
             offset_z = -0.25 if vz >= 0 else 0.25
             ax.text(x + offset_x, vz * vector_display_scale + offset_z, label_text, fontsize=7, color='black', ha='right', va='top')
@@ -200,7 +200,7 @@ if bolts:
         elif view_option == "YZ View":
             ax.quiver(y, 0, vy * vector_display_scale, vz * vector_display_scale, angles='xy', scale_units='xy', scale=1, color='purple')
             ax.plot(y, 0, 'ro')
-            label_text = ' & '.join(f"Bolt {j}" for j in bolt_positions[(round(y, 3), round(vz * vector_display_scale, 3))])
+            label_text = ' & '.join(f"Bolt {j}" for j in bolt_positions[(round(y, 3), round(0, 3))])
             offset_y = -0.25 if vy >= 0 else 0.25
             offset_z = -0.25 if vz >= 0 else 0.25
             ax.text(y + offset_y, vz * vector_display_scale + offset_z, label_text, fontsize=7, color='black', ha='right', va='top')
