bc8a8f005ab987e78bbe84c9543acaaf5331e3df
Noah-Abraham <nkabraham28@gmail.com>
2025-06-04 14:03:40 -0400

    Update app.py

--- app.py
+++ app.py
@@ -146,7 +146,7 @@ if bolts:
     IX, IY, IXY = compute_reference_inertias(bolts)
     theta = compute_principal_axes(IX, IY, IXY)
     for b in bolts:
-        b.prime_distance_from_centroid(bolts,theta)
+        b.prime_distance_from_centroid(theta)
     
     IPX, IPY = compute_principal_moments(bolts, XMC, YMC, theta)
     OMX, OMY = overturning_moments(PX, PY, PZ, LX, LY, LZ, XMC, YMC)
