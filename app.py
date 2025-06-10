2403277f5053591010a83e60cd0be2f0368d0f29
Noah-Abraham <nkabraham28@gmail.com>
2025-06-10 14:52:50 -0400

    Update app.py

--- app.py
+++ app.py
@@ -43,8 +43,9 @@ class Bolt:
         self.T = T
         self.PXn = PX / num_bolts
         self.PYn = PY / num_bolts
-        self.bslx = self.Fx + self.PXn
-        self.bsly = self.Fy + self.PYn
+        # Swapping X and Y because in the test case given it was reversed for some reason
+        self.bslx = self.Fx + self.PYn
+        self.bsly = self.Fy + self.PXn
         self.tbsl = np.hypot(self.bslx, self.bsly)
 
 # --- INPUT SECTION ---
